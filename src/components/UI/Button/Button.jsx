07899b01ce330333336dfd23ba008cb1663a7a1e
segev shoval <segevshoval@segevs-MacBook-Pro.local>
2021-03-01 20:55:12 +0200

    fixed button issue

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -4,8 +4,11 @@ import styles from './button.module.css';
 const Button = (props) => (
 	<button
 		disabled={props.disabled}
-		className={[styles.Button, styles[props.btnType]].join(' ')}
+		className={styles.Button}
 		onClick={props.onClick}
+        id={props.id}
+        style={{ backgroundColor: props.page ? 'rgb(255,204,0)' : 'black'}}
+
 	>
 		{props.children}
 	</button>
--- src/components/UI/Pagination/Pagination.jsx
+++ src/components/UI/Pagination/Pagination.jsx
@@ -1,37 +1,17 @@
 import React from "react";
 import styles from "./pagination.module.css";
+import Button from '../Button/Button'
 
 export default function Pagination({setPageHandler,page,back, forward}) {
 	return (
 		<div style={{ textAlign: "center" }}>
-
-			<button className={styles.pageBtn} onClick={back}>
-				{"<"}
-			</button>
-			<button
-				className={styles.pageBtn}
-				onClick={setPageHandler}
-				id="1"
-				active="true"
-			>
-				1
-			</button>
-			<button className={styles.pageBtn} style={{ backgroundColor: page === 2 ? 'rgb(255,204,0)' : null} }onClick={setPageHandler} id="2">
-				2
-			</button>
-			<button className={styles.pageBtn} onClick={setPageHandler} id="3">
-				3
-			</button>
-			<button className={styles.pageBtn} onClick={setPageHandler} id="4">
-				4
-			</button>
-			<button className={styles.pageBtn} onClick={setPageHandler} id="5">
-				5
-			</button>
-			<button className={styles.pageBtn} onClick={forward}>
-				{" "}
-				{">"}{" "}
-			</button> 
+			<Button onClick={back}> {'<'} </Button>
+			<Button onClick={setPageHandler} id="1"> 1 </Button>
+			<Button onClick={setPageHandler} id="2"> 2 </Button>
+			<Button onClick={setPageHandler} id="3"> 3 </Button>
+			<Button onClick={setPageHandler} id="4"> 4 </Button>
+			<Button onClick={setPageHandler} id="5"> 5 </Button>
+			<Button onClick={back}> {'>'} </Button>
 		</div>
 	);
 }
